ed2e3a5f0e6e86f1f7cfbc156cf2f75a43c7194f
Alvin <alvin.tol@hotmail.com>
2022-09-23 11:01:11 -0600

    adds /graphql route

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+const expressGraphQL = require('express-graphql');
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}))
 
 app.listen(5000, () => console.log('Server Running...'))
