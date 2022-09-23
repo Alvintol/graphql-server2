36a5c1469b4abde9cc0fe227eff5043ccd636770
Alvin <alvin.tol@hotmail.com>
2022-09-23 16:55:37 -0600

    adds mutationQueryFields

--- server.js
+++ server.js
@@ -73,6 +73,8 @@ const bookFields = () => ({
   }
 });
 
+
+
 const rootQueryFields = () => ({
   book: {
     type: BookType,
@@ -106,6 +108,30 @@ const rootQueryFields = () => ({
   },
 });
 
+const mutationQueryFields = () => ({
+  addBook: {
+    type: BookType,
+    description: 'Add a book',
+    args: {
+      name: {
+        type: GraphQLNonNull(GraphQLString)
+      },
+      authorId: {
+        type: GraphQLNonNull(GraphQLInt)
+      }
+    },
+    resolve: (parent, args) => {
+      const book = {
+        id: books.length + 1,
+        name: args.name,
+        authorId: args.authorId
+      };
+      books.push(book)
+      return book
+    }
+  }
+})
+
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
   description: 'This represents an author of a book',
@@ -127,29 +153,7 @@ const RootQueryType = new GraphQLObjectType({
 const RootMutationType = new GraphQLObjectType({
   name: 'Mutation',
   description: 'Root Mutation',
-  fields: () => ({
-    addBook: {
-      type: BookType,
-      description: 'Add a book',
-      args: {
-        name: {
-          type: GraphQLNonNull(GraphQLString)
-        },
-        authorId: {
-          type: GraphQLNonNull(GraphQLInt)
-        }
-      },
-      resolve: (parent, args) => {
-        const book = {
-          id: books.length + 1,
-          name: args.name,
-          authorId: args.authorId
-        };
-        books.push(book)
-        return book
-      }
-    }
-  })
+  fields: mutationQueryFields
 })
 
 const schema = new GraphQLSchema({
