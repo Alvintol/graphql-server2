bee4f56a299aefc2928a958abc02137648ea8e9e
Alvin <alvin.tol@hotmail.com>
2022-09-23 17:28:48 -0600

    migrates type functions into types.js

--- server.js
+++ server.js
@@ -1,35 +1,8 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql').graphqlHTTP
-const {
-  GraphQLSchema,
-  GraphQLObjectType
-} = require('graphql');
+const { GraphQLSchema } = require('graphql');
 const app = express();
 
-
-const AuthorType = new GraphQLObjectType({
-  name: 'Author',
-  description: 'This represents an author of a book',
-  fields: authorFields
-});
-
-const BookType = new GraphQLObjectType({
-  name: 'Book',
-  description: 'This represents a book written by an author',
-  fields: bookFields
-});
-
-const RootQueryType = new GraphQLObjectType({
-  name: 'Query',
-  description: 'Root Query',
-  fields: rootQueryFields
-});
-
-const RootMutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  description: 'Root Mutation',
-  fields: mutationQueryFields
-})
 
 const schema = new GraphQLSchema({
   query: RootQueryType,
